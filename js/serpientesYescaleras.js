7c81da63e566cb03914a63ee84790337f010fad9
DemeGR <gurd970714@gs.utm.mx>
2022-08-09 14:18:27 -0500

    Aprendiendo a tirar dados sin serpientes ni escaleras.

--- js/serpientesYescaleras.js
+++ js/serpientesYescaleras.js
@@ -1,38 +1,40 @@
 //Variables globales 
 let casillasJugador = 0
-let dadoJugador
+let dadoJugador = 0
 
+//Carga todo el contenido del HTML
 window.addEventListener('load', iniciarJuego )
 
+//Solo muestra la parte de seleccionar numero de jugadores
 function iniciarJuego(){
     let sectionPartido = document.getElementById('section-partido')
-    sectionPartido.style.display = 'none'
+    sectionPartido.style.display = 'none'                          //oculta la parte de seccion partido
 
     let sectionReiniciar = document.getElementById('reiniciar')
-    sectionReiniciar.style.diplay = 'none'
+    sectionReiniciar.style.display = 'none'                         //oculta el boton reiniciar
 
     let sectionTirarDado = document.getElementById('tirar-dado')
-    sectionTirarDado.style.diplay = 'none'
+    sectionTirarDado.style.display = 'none'                          //oculta el boton tirar dado        
 
     let botonNumJugador = document.getElementById('boton-jugador')
-    botonNumJugador.addEventListener('click', seleccionarNumJugador)
+    botonNumJugador.addEventListener('click', seleccionarNumJugador)    //escucha el boton seleccionar
 
     let boton1Dado = document.getElementById('1-dado')
-    boton1Dado.addEventListener('click', caraDado)
+    boton1Dado.addEventListener('click', caraDado)                      //escucha el boton tirar dado con un click 
 
-    let botonReiniciar = document.getElementById('boton-reiniciar')
+    let botonReiniciar = document.getElementById('boton-reiniciar')     //escucha el boton reiniciar 
     botonReiniciar.addEventListener('click', reiniciarJuego)
 }
 
 function seleccionarNumJugador(){
     let sectionSeleccionarJugadores = document.getElementById('seleccionar-jugadores')
-    sectionSeleccionarJugadores.style.display = 'none'
+    sectionSeleccionarJugadores.style.display = 'none'              //oculta la parte de selecionar jugadores
 
     let sectionTirarDado = document.getElementById('tirar-dado')
-    sectionTirarDado.style.diplay = 'none'
+    sectionTirarDado.style.display = 'none'                         //oculta la parte de tirar dado
 
     let sectionPartido = document.getElementById('section-partido')
-    sectionPartido.style.display = 'block'
+    sectionPartido.style.display = 'block'                          //muestra la seccion de partido 
 
     let input1Jugador = document.getElementById('1-jugador')
     let spanNumJugar = document.getElementById('1jugador')
@@ -45,8 +47,48 @@ function seleccionarNumJugador(){
     }
 }
 
+
 function caraDado(){
+    let sectionMensaje = document.getElementById('mensaje')
+    let parrafo = document.createElement('p')
+
     dadoJugador = aleatorio(1,6)
+    casillasJugador = casillasJugador + dadoJugador
+
+    if(casillasJugador < 50){
+        parrafo.innerHTML = 'Tiraste un '+ dadoJugador +' y has avanzado ' +casillasJugador +' casillas.'
+    }else{
+        parrafo.innerHTML = 'Tiraste un '+ dadoJugador +' y llegaste a la meta.' 
+    }
+    
+    
+
+    sectionMensaje.appendChild(parrafo)
+
+    meta()
+}
+
+function meta(){
+    if(casillasJugador >= 50){
+        crearMensajeFinal("¡GANASTE!")
+    }else{
+        
+    }
+        
+}
+
+function crearMensajeFinal(resultadoFinal){
+    let sectionMensaje = document.getElementById('mensaje')
+    let parrafo = document.createElement('p')
+    parrafo.innerHTML = resultadoFinal
+
+    sectionMensaje.appendChild(parrafo)
+
+    let boton1Dado = document.getElementById('1-dado')
+    boton1Dado.disabled = true
+
+    let sectionReiniciar = document.getElementById('reiniciar')
+    sectionReiniciar.style.display = 'block'
 }
 
 function aleatorio(min, max){
